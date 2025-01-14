739f9d998b7795263e25d6b67f8dbefec69b5209
SilviuIordache <silviu.iordache92@gmail.com>
2025-01-14 17:24:11 +0200

    Add photo client

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from 'react';
+import getPhotos from '../../services/photo/getPhotos';
+import type { PhotosWithTotalResults, ErrorResponse } from 'pexels';
+
 const Gallery = () => {
+  const [photos, setPhotos] = useState<
+    PhotosWithTotalResults | ErrorResponse | null
+  >(null);
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const result = await getPhotos('nature');
+      setPhotos(result);
+    };
+    fetchPhotos();
+  }, []);
+
+  console.log(photos);
   return <div className="bg-gray-200">Gallery</div>;
 };
 
---
+++ src/services/photo/getPhotos.ts
@@ -0,0 +1,11 @@
+import client from './photoClient';
+
+export default async function getPhotos(query: string) {
+  try {
+    const result = await client.photos.search({ query, per_page: 1 });
+    return result;
+  } catch (error) {
+    console.error('Error fetching photos:', error);
+    throw error;
+  }
+}
