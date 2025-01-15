c506adf9761e7ed01e76b2204e2868c91013e45f
SilviuIordache <silviu.iordache92@gmail.com>
2025-01-15 16:27:32 +0200

    Fix bug where the gallery dissappears

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -8,8 +8,9 @@ const Gallery = () => {
   const [allPhotos, setAllPhotos] = useState<PhotosWithTotalResults['photos']>(
     []
   );
+  const [isFetchingMore, setIsFetchingMore] = useState(false);
 
-  const { data, isLoading, error } = usePhotosQuery({
+  const { data, error } = usePhotosQuery({
     query: 'animals',
     per_page: 5,
     page: page,
@@ -17,33 +18,38 @@ const Gallery = () => {
 
   useEffect(() => {
     if (data && 'photos' in data) {
-
       setAllPhotos((prevPhotos) => [...prevPhotos, ...data.photos]);
+      setIsFetchingMore(false);
     }
   }, [data]);
 
-  if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
-  if (!data || !('photos' in data)) return <div>No photos found</div>;
 
   const loadMoreImages = () => {
     console.log('Load more images button clicked');
-
+    setIsFetchingMore(true);
     setPage((prevPage) => prevPage + 1);
   };
 
   return (
     <div>
       <div className="masonry-grid">
-        {allPhotos.map((photo) => (
-          <GalleryImage key={photo.id} photo={photo} />
-        ))}
+        {allPhotos.length > 0 ? (
+          allPhotos.map((photo) => (
+            <GalleryImage key={photo.id} photo={photo} />
+          ))
+        ) : (
+          <div>No photos found</div>
+        )}
       </div>
+
+      {isFetchingMore && <div>Loading more images...</div>}
 
       <div className="flex justify-center">
         <button
           className="btn bg-blue-500 text-white hover:bg-blue-600 p-2 rounded-md mt-4"
           onClick={loadMoreImages}
+          disabled={isFetchingMore}
         >
           Load more images
         </button>
