28a3075c21538e10dcf75ba23c495f055055e07a
SilviuIordache <silviu.iordache92@gmail.com>
2025-01-15 22:58:04 +0200

    Add trick to load higher quality images after a while

--- src/components/gallery/GalleryImage.tsx
+++ src/components/gallery/GalleryImage.tsx
@@ -1,5 +1,6 @@
 import { Photo } from 'pexels';
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 interface GalleryImageProps {
   photo: Photo;
@@ -7,6 +8,18 @@ interface GalleryImageProps {
 
 const GalleryImage = ({ photo }: GalleryImageProps) => {
   const navigate = useNavigate();
+  const [loadHigherQuality, setLoadHigherQuality] = useState(false);
+
+  // after a few seconds, load higher quality image
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoadHigherQuality(true);
+    }, 3000);
+
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, []);
+
+  const imageSrc = loadHigherQuality ? photo.src.large : photo.src.small;
 
   const handleClick = () => {
     navigate(`/${photo.id}`);
@@ -15,11 +28,10 @@ const GalleryImage = ({ photo }: GalleryImageProps) => {
   return (
     <div onClick={handleClick} className="mb-4">
       <img
-        src={photo.src.small}
+        src={imageSrc}
         alt={photo.alt || ''}
-        className="w-full rounded-lg hover:opacity-80 hover:cursor-pointer transform transition-transform duration-200 hover:scale-105"
-        width={photo.width}
-        height={photo.height}
+        className="w-full h-auto rounded-lg hover:opacity-80 hover:cursor-pointer transform transition-transform duration-200 hover:scale-105"
+        style={{ width: '300px', height: 'fit-content', objectFit: 'cover' }}
       />
     </div>
   );
