c65201365c1bde1b88fce74cfb6968713e61fd09
SilviuIordache <silviu.iordache92@gmail.com>
2025-01-16 17:12:56 +0200

    Remove old optimisations

--- src/components/gallery/GalleryImage.tsx
+++ src/components/gallery/GalleryImage.tsx
@@ -8,33 +8,15 @@ interface GalleryImageProps {
 
 const GalleryImage = ({ photo }: GalleryImageProps) => {
   const navigate = useNavigate();
-  // const [loadHigherQuality, setLoadHigherQuality] = useState(false);
-
-  // after a few seconds, load higher quality image
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setLoadHigherQuality(true);
-  //   }, 2000);
-
-  //   return () => clearTimeout(timer); // Cleanup the timer on component unmount
-  // }, []);
-
-  const imageSrc = photo.src.medium;
 
   const handleClick = () => {
     navigate(`/${photo.id}`);
   };
 
   return (
-    <div
-      onClick={handleClick}
-      // onMouseEnter={() => {
-      //   setLoadHigherQuality(true);
-      // }}
-      className="mb-4"
-    >
+    <div onClick={handleClick} className="mb-4">
       <img
-        src={imageSrc}
+        src={photo.src.medium}
         alt={photo.alt || ''}
         className="w-full h-auto rounded-lg hover:opacity-80 hover:cursor-pointer transform transition-transform duration-200 hover:scale-105"
         style={{ width: '300px', height: 'fit-content', objectFit: 'cover' }}
