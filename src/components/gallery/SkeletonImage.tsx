3afa9e20f0616244a1ea44ec65da831aa18f2ea5
SilviuIordache <silviu.iordache92@gmail.com>
2025-01-17 16:15:25 +0200

    Adjust SkeletonImage

--- src/components/gallery/SkeletonImage.tsx
+++ src/components/gallery/SkeletonImage.tsx
@@ -1,15 +1,16 @@
 const SkeletonImage = () => {
-  const minHeight = 10;
-  const maxHeight = 20;
+  const minHeight = 25;
+  const maxHeight = 30;
 
-  const width = '19rem';
   const height = `${
     Math.floor(Math.random() * (maxHeight - minHeight + 1)) + minHeight
   }rem`;
 
+  const width = '18.5rem';
+
   return (
     <div
-      className=" w-full bg-gray-500 rounded-md"
+      className=" w-full bg-gray-300 rounded-md"
       style={{ height, width }}
     ></div>
   );
