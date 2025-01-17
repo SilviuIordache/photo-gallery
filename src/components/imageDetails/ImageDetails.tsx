46bcaa2b0a6f0eb96b3d5034311e793f4a93accd
SilviuIordache <silviu.iordache92@gmail.com>
2025-01-17 16:25:23 +0200

    Navigate correctly

--- src/components/imageDetails/ImageDetails.tsx
+++ src/components/imageDetails/ImageDetails.tsx
@@ -21,7 +21,7 @@ const ImageDetails = (props: Props) => {
       <div className="flex justify-between items-start">
         <button
           className="bg-blue-500 text-white px-4 py-2 rounded-md fixed top-0 left-0 m-4"
-          onClick={() => navigate('/')}
+          onClick={() => navigate(-1)}
         >
           Go Back
         </button>
